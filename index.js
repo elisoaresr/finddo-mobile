324baff5eef8e19e0caa0d6025d4abc5de212a19
Eliezer Soares <eliezerdrum@gmail.com>
2020-08-03 17:03:45 -0300

    Mobile Application

--- index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import Home from './src/pages/Home';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => Home);
